460868f9fadebf8b656cc2c319669fb4bc4a3045
Rob_0.0.7 <robmuradyann@gmail.com>
2025-09-03 14:33:23 +0400

    Save work before switching branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/HomePage/Home';
+import './style.css';
+
+import Header from './widgets/header/Header';
+import About from './widgets/pages/About/About';
+import Menu from './widgets/pages/Menu/Menu';
+import Contact from './widgets/pages/Contact/Contact';
+import Cart from './widgets/pages/Cart/Cart';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        
+        <Route path='/' element={<Home />} />
+      
+        <Route path='/' element={<About />}/>
+        <Route path="/Menu" element={<Menu />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Cart" element={<Cart />} />
       </Routes>
     </Router>
   );
