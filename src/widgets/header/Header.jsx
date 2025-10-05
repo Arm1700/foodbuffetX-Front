f231a1312496ea2bcf9a825ba727d68cfd1f95e5
esimchgidem07-sudo <esimchgidem07@gmail.com>
2025-10-05 18:38:03 +0400

    footer contact

--- src/widgets/header/Header.jsx
+++ src/widgets/header/Header.jsx
@@ -1,8 +1,6 @@
 import { Link } from "react-router-dom";
 export default function Header() {
-
   return (
-<<<<<<< HEAD
     <div className="fixed top-0 left-0 w-full z-50 bg-black text-white p-4 flex justify-around items-cente">
 
   
@@ -17,23 +15,6 @@ export default function Header() {
             </Link>  
             <Link to="/Contact">Contact</Link>
             <Link to="/Cart">Cart</Link>
-=======
-    <div className="bg-black text-white p-4 flex justify-around items-center absolute w-full top-0 z-10">
-        <div className="flex space-x-6">
-          <Link to="/About">About</Link>
-          <Link to="/Menu">Menu</Link>
-
-          <Link to="/">     
-          <img   src="https://cdn.prod.website-files.com/6648175f98e8b71e60b3e986/664bbc6fee6471421c62c3b9_logo-web-white-foodbuffet-x-webflow-template.svg" alt="logo" className="h-7 " />
-    
-          </Link>  
-          <Link to="/Contact">Contact</Link>
-          <Link to="/Cart">Cart</Link>
-      </div>
-
-      <div>
-        <Link to="/Account">Login/Register</Link>
->>>>>>> 545554c354d1b124c2e674e5e67592bc9a94c13b
       </div>
     </div>
   )
