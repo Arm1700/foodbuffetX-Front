267efd9308363e1869f788ab00b6b0a266c5e8e3
Arm1700 <bukboks1@gmail.com>
2025-11-30 20:23:43 +0400

    home

--- src/pages/HomePage/povr.jsx
+++ src/pages/HomePage/povr.jsx
@@ -1,21 +1,12 @@
 export default function Povr() {
     return (
-        <div>
-            <div className="relative p-20  bg-[#f7f7f7]">
+        <div className=" p-10  bg-[#f7f7f7] ">
+            <div className="relative max-w-[1280px] mx-auto">
                 <img className=" absolute -z-10 w-[16%] h-[40%] top-[900px] left-[100px]" src="/klor2.svg" alt="" />
-                <div className="grid lg:grid-cols-2 gap-16">
-                    <div className="space-y-8">
-                        <img className="w-[610px] h-[758px] object-cover" src="https://cdn.prod.website-files.com/6648175f98e8b71e60b3e986/664ccb67504a0403838bc2d6_manhattan-new-york-chef-foodbuffet-x-webflow-template.jpg" alt="Manhattan Chef" />
-                        <div className="space-y-2">
-                            <p className="text-black  text-[28px]">Manhattan, New York</p>
-                            <div className="flex items-center gap-4">
-                                <p className="text-gray-500 text-[18px]">+1 (212) 857 - 0107</p>
-                                <div className="w-px h-6 bg-gray-300"></div>
-                                <p className="text-gray-500 text-[18px]">NEWYORK@FOODBUFFET.COM</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="space-y-8">
+                {/* <img className=" text-red-500 absolute w-[40%] top-[320px] left-[380px]" src="/klor.svg" alt="" /> */}
+                <div className="grid lg:grid-cols-[1fr_40%] grid-cols-[1fr_1fr] gap-8">
+                    <img className="lg:row-span-2 lg:w-[610px] lg:h-[758px] w-full h-[480px] object-cover" src="https://cdn.prod.website-files.com/6648175f98e8b71e60b3e986/664ccb67504a0403838bc2d6_manhattan-new-york-chef-foodbuffet-x-webflow-template.jpg" alt="Manhattan Chef" />
+                    <div className="space-y-8 row-1 col-span-2 lg:col-2">
                         <h2 className=" text-[72px] font-bold text-black font-serif">About us</h2>
                         <p className="text-gray-600 text-[18px] leading-relaxed">
                             Lorem ipsum dolor sit amet consectetur adipiscing elit ugue quam diam vitae velit bibendum elementum eget non vivamus volutpat odio cras vestibulum lacus ut. In consequat.
@@ -28,9 +19,15 @@ export default function Povr() {
                                 Browse Menu
                             </button>
                         </div>
-                        <div className="relative ">
-                            <img className=" w-[78%] h-[470px] z-20 object-cover relative rounded-lg" src="https://cdn.prod.website-files.com/6648175f98e8b71e60b3e986/664ccb67c1d31fa999787c13_delicious-sushi-foodbuffet-x-webflow-template.jpg" alt="Sushi Platter" />
-                            <img className=" text-red-500 absolute w-[40%] top-[320px] left-[380px]" src="/klor.svg" alt="" />
+
+                    </div>
+                    <img className="lg:row-span-2  self-end w-full lg:h-[420px] h-[480px] z-20 object-cover relative rounded-lg" src="https://cdn.prod.website-files.com/6648175f98e8b71e60b3e986/664ccb67c1d31fa999787c13_delicious-sushi-foodbuffet-x-webflow-template.jpg" alt="Sushi Platter" />
+                    <div className="space-y-2 row-3">
+                        <p className="text-black  text-[28px]">Manhattan, New York</p>
+                        <div className="flex items-center gap-4">
+                            <p className="text-gray-500 text-[18px]">+1 (212) 857 - 0107</p>
+                            <div className="w-px h-6 bg-gray-300"></div>
+                            <p className="text-gray-500 text-[18px]">NEWYORK@FOODBUFFET.COM</p>
                         </div>
                     </div>
                 </div>
