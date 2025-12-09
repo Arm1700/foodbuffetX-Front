da7f98cf24d1fc09c2c0d3ede3f5d4d30ac56eb9
Rob_0.0.7 <robmuradyann@gmail.com>
2025-12-09 18:33:01 +0400

    Contact.jsx

--- src/widgets/footer/Footer.jsx
+++ src/widgets/footer/Footer.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <footer className="bg-black text-white py-12  px-6  mt-20">
-      <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-4 gap-10   grid-cols-subgrid  ">
+      <div className="max-w-[7xl] mx-auto px-6 grid md:grid-cols-4 gap-10   grid-cols-subgrid  ">
         {/* Left Section */}
         <div className="  h-50 border-gray-800 pr-6">
           <div className="flex items-center gap-2 mb-4 ">
